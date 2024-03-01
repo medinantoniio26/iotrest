fecb4036fadda27338be3441b4fd750331eab05c
Antonio <antonio.jose.medina.e@gmail.com>
2024-03-01 13:00:35 -0600

    ...

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,12 +12,11 @@ class AuthController extends Controller
 {
     public function login(Request $request){
         $user = User::where('username', $request->username)->first();
-        //if(!user) return response('', 404);
         if(!password_verify($request->password, $user->password)) return response('', 401);
         $payload = [
             'sub' => $user->id,
             'iat' => time(),
-            'nbf' => time() + 60*60*24*30
+            'exp' => time() + 60*60*24*30
         ];
         $jwt = JWT::encode($payload, env('JWT_SECRET'), 'HS256');
         return $jwt;
